406fc626eabd867cc8d0e3588a296b0a0b2c129f
Pomdorka <andreychetvertakov@mail.ru>
2020-10-07 02:42:05 +0300

    Create main.c

---
+++ Graph/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+
+struct node{
+    char val[100];
+    char path[10][100];
+    int lim;
+};
+
+struct node nodes[100];
+int N;
+
+int findOrCreate(char *val){
+    for(int i=0; i<N; i++){
+        if(strcmp(nodes[i].val, val)==0){
+            return i;
+        }
+    }
+    strcpy(nodes[N].val, val);
+    N++;
+    return (N-1);
+}
+
+int findPath(int n1, int n2){
+    for(int j=0; j<nodes[n1].lim; j++){
+        if(strcmp(nodes[n1].path[j], nodes[n2].val)==0){
+            printf("Path already exists: %s -- %s\n", nodes[n1].val, nodes[n2].val);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void addPath(int n1, int n2){
+    strcpy(nodes[n1].path[nodes[n1].lim], nodes[n2].val);
+    nodes[n1].lim++;
+}
+
+void dump(){
+    /*printf("graph{\n");
+    for(int i=0; i<N; i++){
+        for(int j=0; j<nodes[i].lim; j++){
+                printf("%s -- %s;\n", nodes[i].val, nodes[i].path[j]);
+        }
+    }
+    printf("}\n");*/
+
+    FILE *ptrFile = fopen("file.dot", "w+");
+    fputs("graph{\n", ptrFile);
+    for(int i=0; i<N; i++){
+        for(int j=0; j<nodes[i].lim; j++){
+                fputs(nodes[i].val, ptrFile);
+                fputs(" -- ", ptrFile);
+                fputs(nodes[i].path[j], ptrFile);
+                fputs(";\n", ptrFile);
+        }
+    }
+    fputs("}\n", ptrFile);
+
+}
+
+int main(int argc, char *argv[]){
+    char *a, *b, tmp[256];
+    for (int i=0; i<100; i++){
+        nodes[i].lim=0;
+    }
+    printf("Paths: \n");
+
+    while(1){
+        scanf("%s", tmp);
+        if(tmp[0]=='!')
+            break;
+        a = strtok(tmp, "-");
+        b = strtok(NULL, "-");
+        int n1=findOrCreate(a);
+        int n2=findOrCreate(b);
+
+        if(!findPath(n1, n2) && !findPath(n2, n1)){
+            addPath(n1, n2);
+        }
+    }
+    dump();
+    system("dot -Tpng C:\DZ_2\file.dot -o demo_dot.png");
+
+    return 0;
+}
